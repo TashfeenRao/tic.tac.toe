062282182368f16d41f3576f7210d9826b1d276f
certilremy <certilremy@gmail.com>
2020-04-22 13:06:19 -0400

    add players object

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,40 @@
-function GameBoard() {
-  gameboard = ["O", "O", "O", "X", "0", "0", "0", "0", "O"];
-  return { gameboard };
-}
+const players = ((name, token)=>{
+  return {name, token}
+})();
 
-function players(name) {
-  return { name };
-}
-
-const tictactoe = GameBoard();
-
-displayBoard(tictactoe.gameboard);
-
-function displayBoard(board) {
-  let table_data = document.getElementsByClassName("table-data")[0];
-  const table = document.createElement("table");
-  table.innerHTML = " ";
-  table.innerHTML = `
+const GameBoard = ( ()=>{
+  const board= [' ', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+ 
+  function displayBoard() {
+    let table_data = document.getElementsByClassName("table-data")[0];
+    const table = document.createElement("table");
+    table.innerHTML = " ";
+    table.innerHTML = `
+      <tr>
+      <td>${this.board[0]}</td>
+      <td class="vert">${this.board[1]}</td>
+      <td>${this.board[2]}</td>
+    </tr>
     <tr>
-    <td>${board[0]}</td>
-    <td class="vert">${board[1]}</td>
-    <td>${board[2]}</td>
-  </tr>
-  <tr>
-    <td class="hori">${board[3]}</td>
-    <td class="vert hori">${board[4]}</td>
-    <td class="hori">${board[5]}</td>
-  </tr>
-  <tr>
-    <td>${board[6]}</td>
-    <td class="vert">${board[7]}</td>
-    <td>${board[8]}</td>
-  </tr>  
-    `;
-  table_data.append(table);
-}
+      <td class="hori">${this.board[3]}</td>
+      <td class="vert hori">${this.board[4]}</td>
+      <td class="hori">${this.board[5]}</td>
+    </tr>
+    <tr>
+      <td>${this.board[6]}</td>
+      <td class="vert">${this.board[7]}</td>
+      <td>${this.board[8]}</td>
+    </tr>  
+      `;
+    table_data.append(table);
+  }
+
+return {board, displayBoard, clearBoard}
+})();
+
+
+GameBoard.displayBoard()
+GameBoard.clearBoard();
+
+// play 
+
