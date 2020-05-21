2d1364ab68e05963d32fd10f58848d3351720b69
tashfeenrao <raotashfeen25@gmail.com>
2020-05-22 02:15:43 +0500

    added test for players token

--- tests/player.test.js
+++ tests/player.test.js
@@ -8,3 +8,12 @@ test('return name of player', () => {
   expect(player1).toEqual('tashfeen');
   expect(player2).toEqual('remi');
 });
+
+test('return token X or O', () => {
+  players.token = 'X';
+  const token1 = players.token;
+  players.token = 'O';
+  const token2 = players.token;
+  expect(token1).toEqual('X');
+  expect(token2).toEqual('O');
+});
