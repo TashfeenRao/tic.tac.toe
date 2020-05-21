d2832167186bae34bdd459c72e595c41c368ec9f
tashfeenrao <raotashfeen25@gmail.com>
2020-05-22 02:45:37 +0500

    added tests for players and gamel logic

--- tests/gameLogic.test.js
+++ tests/gameLogic.test.js
@@ -1,4 +1,5 @@
 import GameLogic from '../src/gameLoginc';
+import GameBoard from '../src/gameBorad';
 
 test('check win possibilities', () => {
   expect(GameLogic.WIN_POSSIBILITY.length).toEqual(8);
@@ -6,4 +7,10 @@ test('check win possibilities', () => {
 
 test('check the freeBoard', () => {
   expect(GameLogic.freeBoard().length).toEqual(9);
+});
+
+test('check for is there a win', () => {
+  GameBoard.board = ['X', 'X', 'X', '', '', '', '', '', ''];
+  GameLogic.chekwin();
+  expect(GameLogic.winArr.length).toEqual(1);
 });
