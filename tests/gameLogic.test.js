2acdfbb13c5f5629a95089f7386f386f11c771b0
tashfeenrao <raotashfeen25@gmail.com>
2020-05-22 02:23:08 +0500

    update the check possibilties test

--- tests/gameLogic.test.js
+++ tests/gameLogic.test.js
@@ -1,14 +1,5 @@
 import GameLogic from '../src/gameLoginc';
 
 test('check win possibilities', () => {
-  expect(GameLogic.WIN_POSSIBILITY).toEqual([
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]);
+  expect(GameLogic.WIN_POSSIBILITY.length).toEqual(8);
 });
