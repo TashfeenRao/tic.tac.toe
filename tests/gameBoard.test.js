6eaf96b53ecd2d8850198aea25827ca49287418f
certilremy <certilremy@gmail.com>
2020-05-21 16:04:59 -0400

    add test for board

---
+++ tests/gameBoard.test.js
@@ -0,0 +1,15 @@
+import gameBorad from '../src/gameBorad';
+
+test('The board should load empy', () => {
+  expect(gameBorad.board).toEqual(['', '', '', '', '', '', '', '', '']);
+});
+
+test('The board should be an array with 9 element', () => {
+  expect(gameBorad.board.length).toBe(9);
+});
+
+test('Clear the board', () => {
+  gameBorad.board[1] = 'X';
+  gameBorad.clearBoard();
+  expect(gameBorad.board).toEqual(['', '', '', '', '', '', '', '', '']);
+});
